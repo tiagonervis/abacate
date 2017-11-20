71858f190277e234d44a527010402b0269093290
Tiago Nervis <tiagonervis@gmail.com>
2017-11-20 07:45:22 -0200

    Implementados 2 testes unitarios da api com o mocha

--- test.js
+++ test.js
@@ -10,114 +10,117 @@ var urlApi = 'https://abacate.herokuapp.com/app/';
 var usuario = 'admin';
 var senha = 'admin';
 
-var buffer = new Buffer(usuario + ':' + senha);
-var auth = buffer.toString('base64');
+//Objeto de fachadas para testes
+var fachadas = {
+  equipamentos: {descricao: ''},
+  locais: {local: ''},
+  registros_logs: {tabela: ''},
+  marcas: {descricao: '' },
+  pontos: {codPonto: '' },
+  racks: {identificacao: '' },
+  responsaveis: {nome: '' },
+  situacoes: {situacao: '' },
+  solicitacoes: {ponto: { codPonto: ''} },
+  cabling: {cabling: '' },
+  tipos_equipamentos: {tipo: '' },
+  tipos_racks: {tipo: '' },
+  tipos_servicos: {tipo: '' },
+  tipos_solicitacoes: {tipo: '' },
+  terminacoes: {terminacao: '' },
+  usuarios: {nome: '' }
+};
 
-var models = [];
+//Teste 1
+describe('1 - Consultando acesso sem autenticação:', function() {
 
-describe('Executando testes:', function () {
-
+  //Define tempo maximo para expirar
   this.timeout(20000);
 
-  after(function(done) {
-
-    fs.readdir('models', function (erro, arquivos) {
-
-      var pendentes = arquivos.length;
-      for (var i in arquivos) {
-
-        let arquivo = arquivos[i];
-
-        fs.readFile('models/' + arquivo, 'utf8', function(erro, conteudo) {
-
-          models.push(JSON.parse(conteudo));
-
-          pendentes--;
-
-          if (pendentes === 0) {
-            done();
-          }
-        });
-      }
-    });
-
-  });
-
- it('teste', function() {
-
-   console.log(models);
-   for (var i in models) {
-
-     let model = models[i];
-
-     it(model[i], function (done) {
-
-         url = model.url + '/pesquisar?pagina=0&quantidade=10';
-
-         obj = {};
-         obj[model.busca.campo] = '';
-
-         consultaApi(url, 'POST', null, obj, function (res) {
-           assert.equal(res.statusCode, 500, res.statusMessage);
-           done();
-         });
-       });
-   }
- });
-
-});
-
-/*
-
-describe('3 - Consultando acesso do usuário:', function() {
-
-  this.timeout(20000);
-
+  //Percorre todas as fachadas
   for (let fachada in fachadas) {
 
+    //Executa testa da fachada atual
     it(fachada, function (done) {
 
-      let model = fachadas[fachada];
+      //Monta url de pesquisa
+      let url = fachada + '/pesquisar?pagina=0&quantidade=10';
 
-      url = fachada + '/pesquisar?pagina=0&quantidade=10';
+      //Executa consulta api
+      consultaApi(url, 'POST', null, fachadas[fachada], function (res) {
 
-      obj = {};
-      obj[model.busca.campo] = '';
+        //Verifica se recebeu resposta 500
+        assert.equal(res.statusCode, 500, res.statusMessage);
 
-      consultaApi(url, 'POST', auth, obj, function (res) {
-        assert.equal(res.statusCode, 200, res.statusMessage);
+        //Finaliza o teste
         done();
       });
     });
   }
 });
-*/
-function carregarModel(arquivo, callback) {
-  fs.readFile(arquivo, 'utf8', function(erro, conteudo) {
-    if (!erro) {
-      callback(JSON.parse(conteudo));
-    }
-  });
-}
 
+//Teste 2
+describe('2 - Consultando acesso com autenticação:', function() {
+
+  //Define tempo maximo para expirar
+  this.timeout(20000);
+
+  //Converte usuario e senha em base64
+  var buffer = new Buffer(usuario + ':' + senha);
+  var auth = buffer.toString('base64');
+
+  //Percorre todas as fachadas
+  for (let fachada in fachadas) {
+
+    //Executa testa da fachada atual
+    it(fachada, function (done) {
+
+      //Monta url de pesquisa
+      let url = fachada + '/pesquisar?pagina=0&quantidade=10';
+
+      //Executa consulta api
+      consultaApi(url, 'POST', auth, fachadas[fachada], function (res) {
+
+        //Verifica se recebeu resposta 200
+        assert.equal(res.statusCode, 200, res.statusMessage);
+
+        //Finaliza o teste
+        done();
+      });
+    });
+  }
+});
+
+//Funcao para consulta a api
 function consultaApi(fachada, metodo, auth, dados, callback) {
 
+  //Define configuracoes da requisicao
   let configs = {
     method: metodo,
     url: urlApi + fachada,
     headers: {}
   };
 
+  //Se foi informada autenticacao
   if (auth !== null) {
+
+    //Define autenticacao no cabecalho autorization
     configs.headers['Authorization'] = auth;
   }
 
+  //Se dados foram informados
   if (dados !== null) {
+
+    //Define tipo de conteudo json
     configs.headers['Content-Type'] = 'application/json';
+
+    //Insere dados no corpo
     configs.json = dados;
   }
 
+  //Executa requisicao
   request(configs, function (err, res, body) {
+
+    //Chama callback com resposta
     callback(res);
   });
 };
